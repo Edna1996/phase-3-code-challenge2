6448b96b537fbc9a9563220bc8ff8f5df31ef354
edna1996 <guantaiedna@gmapl.com>
2023-02-28 18:44:46 +0300

    created a product class that inherits from active record

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,36 +1,35 @@
-# This will delete any existing rows from the Product and User tables
-# so you can run the seed file multiple times without having duplicate entries in your database
-puts "Deleting old data..."
-Product.destroy_all
-User.destroy_all
-
-puts "Creating users..."
-user1 = User.create(name: Faker::Name.name)
-user2 = User.create(name: Faker::Name.name)
-user3 = User.create(name: Faker::Name.name)
-
-puts "Creating products..."
-product1 = Product.create(name: "Stapler", price: 10)
-product2 = Product.create(name: "Whiteboard", price: 15)
-product3 = Product.create(name: "Dry Erase Markers", price: 5)
-product4 = Product.create(name: "Ballpoint Pens", price: 2)
-product5 = Product.create(name: "Scotch Tape", price: 3)
-
-puts "Creating reviews..."
-# *****************************************************************
-# * TODO: create reviews! Remember, a review belongs to a product *
-# * and a review belongs to an user.                              *
-# *****************************************************************
-# Create Reviews Here
-15.times do 
-    user_ids = User.all.map {|user| user.id}
-    product_ids = Product.all.map {|product| product.id}
-
-    Review.create(
-        user_id: user_ids[rand(0..2)],
-        product_id: product_ids[rand(0..4)],
-        star_rating: rand(1..10),
-        comment: Faker::Lorem.sentence(word_count: rand(1...10))
+puts "🌱 Seeding data..."
+# run a loop 50 times
+50.times do
+    # create a game with random data
+    product = Product.create(
+      name: Faker::Commerce.product_name,
+      price: rand(0..60), # random number between 0 and 60
     )
-end
-puts "Seeding done!"
+
+    # # create between 1 and 5 reviews for each product
+    rand(1..5).times do
+      Review.create(
+        product_id: product.id,
+        star_rating: rand(1..5),
+        comment: Faker::Lorem.sentence         
+      )
+    end
+  end
+  # create users
+  50.times do
+    user = User.create(
+      name: Faker::Name.name
+    )
+  end
+
+  # User#reviews
+  user1 = User.first
+  review2 = Review.second
+  user1.reviews << review2
+
+  user2 = User.second
+  review1 = Review.first
+  user2.reviews << review1
+
+puts "🌱 Done seeding!"
